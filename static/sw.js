f5810713ff037bc1c2f97b0296d67a3d56d43aa8
Karunesh P <karuneshp2007@gmail.com>
2026-04-27 00:00:20 +0530

    Add files via upload

---
+++ static/sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'caltrack-lite-v1';
+const ASSETS = [
+    '/',
+    '/static/style.css',
+    'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+// Activate Service Worker
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+            );
+        })
+    );
+});
+
+// Fetch Strategy: Network First, falling back to cache
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
+        })
+    );
+});
